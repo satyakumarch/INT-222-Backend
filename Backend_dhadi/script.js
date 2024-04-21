6bda8e92619cc34d5937259474c518bc139f7bcd
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-04-21 15:12:38 +0530

    first commit

---
+++ Backend_dhadi/script.js
@@ -0,0 +1,11 @@
+//fundamental of javascript
+//array and object
+//function return
+//async js coding
+//foreach map filter find indexOf
+
+//for each
+var arr=[1,2,3,4,5];
+arr.forEach(function(val){
+    console.log(val+" hello ");
+})
